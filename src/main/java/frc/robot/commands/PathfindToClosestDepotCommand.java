81f1990a20eab881337b934020d56df3f096a816
vortechs <jobartbenjamin@gmail.com>
2025-01-25 14:52:00 -0800

    THIS BREAKS TEH ROBOT

--- src/main/java/frc/robot/commands/PathfindToClosestDepotCommand.java
+++ src/main/java/frc/robot/commands/PathfindToClosestDepotCommand.java
@@ -31,9 +31,7 @@ public class PathfindToClosestDepotCommand extends Command {
 
   // Called when the command is initially scheduled.
   @Override
-  public void initialize() {
-    depotPathCommands = PathfindingCommands.getPathfindingCommands();
-  }
+  public void initialize() {}
 
   // Called every time the scheduler runs while the command is scheduled.
   @Override
@@ -42,6 +40,7 @@ public class PathfindToClosestDepotCommand extends Command {
     System.out.println("testposeID: " + testPoseID);
 
     if (!lockedIn) {
+      depotPathCommands = PathfindingCommands.getPathfindingCommands();
       depotPathCommands[targetPoseID].schedule();
       lockedIn = true;
     }
