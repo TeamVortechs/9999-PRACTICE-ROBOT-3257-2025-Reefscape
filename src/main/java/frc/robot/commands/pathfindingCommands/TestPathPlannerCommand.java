5e253e899ba89fc7be94b37ba98e9bdf07b92742
kolako <sylin54#3685>
2025-02-20 10:41:03 -0800

    thought of a new idea so commiting this rn

--- src/main/java/frc/robot/commands/pathfindingCommands/TestPathPlannerCommand.java
+++ src/main/java/frc/robot/commands/pathfindingCommands/TestPathPlannerCommand.java
@@ -1,6 +1,20 @@
 package frc.robot.commands.pathfindingCommands;
 
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Random;
+
+import com.pathplanner.lib.auto.AutoBuilder;
+import com.pathplanner.lib.path.GoalEndState;
+import com.pathplanner.lib.path.PathConstraints;
+import com.pathplanner.lib.path.PathPlannerPath;
+import com.pathplanner.lib.path.Waypoint;
+
+import edu.wpi.first.math.geometry.Pose2d;
+import edu.wpi.first.math.geometry.Rotation2d;
 import edu.wpi.first.wpilibj2.command.Command;
+import edu.wpi.first.wpilibj2.command.InstantCommand;
+import frc.robot.subsystems.drive.Drive;
 
 /*
 Names
@@ -14,17 +28,40 @@ public class TestPathPlannerCommand extends Command {
    *
    * @param subsystem The subsystem used by this command.
    */
-  public TestPathPlannerCommand() {
+
+    private Drive drive;
+    private boolean ranCommand = false;
+    private Command command = new InstantCommand();
+
+
+  public TestPathPlannerCommand(Drive drive) {
     // addRequirements(null);
+    this.drive = drive;
+    
   }
 
   // Called when the command is initially scheduled.
   @Override
-  public void initialize() {}
+  public void initialize() {
+    List<Waypoint> waypoints = PathPlannerPath.waypointsFromPoses(
+        new Pose2d(5.782, 4.190, Rotation2d.fromDegrees(180)), new Pose2d(5.782, 4.190, Rotation2d.fromDegrees(180))
+    );
+
+    PathConstraints pathConstraints = new PathConstraints(3.0, 3.0, 2 * Math.PI, 4 * Math.PI);
+
+    PathPlannerPath path = new PathPlannerPath(waypoints, pathConstraints, null, new GoalEndState(0.0, Rotation2d.fromDegrees(-180)));
+    
+    command = AutoBuilder.followPath(path);
+
+    ranCommand = false;
+  }
 
   // Called every time the scheduler runs while the command is scheduled.
   @Override
-  public void execute() {}
+  public void execute() {
+    ranCommand = true;
+    AutoBuilder.followPath()
+  }
 
   // Called once the command ends or is interrupted.
   @Override
