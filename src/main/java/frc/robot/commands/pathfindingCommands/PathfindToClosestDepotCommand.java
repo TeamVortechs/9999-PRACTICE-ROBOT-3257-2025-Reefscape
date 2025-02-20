353bc8a475a80cb25988e346dc69d882a74276a3
kolako <sylin54#3685>
2025-02-20 11:15:54 -0800

    working version

--- src/main/java/frc/robot/commands/pathfindingCommands/PathfindToClosestDepotCommand.java
+++ src/main/java/frc/robot/commands/pathfindingCommands/PathfindToClosestDepotCommand.java
@@ -74,9 +74,14 @@ public class PathfindToClosestDepotCommand extends Command {
   @Override
   public void end(boolean interrupted) {
     // Cancel the active command if it is still scheduled.
-    // if (activeCommand != null && activeCommand.isScheduled()) {
-    activeCommand.cancel();
-    // }
+    System.out.println(
+        "in the end function of the pathfind to the closest depot command\n\n\n\n\n\n");
+
+    if (activeCommand != null && activeCommand.isScheduled()) {
+      activeCommand.cancel();
+
+      System.out.println("ending the pathfind to closest depot command\n\n\n\n\n\n\n\n");
+    }
   }
 
   @Override
