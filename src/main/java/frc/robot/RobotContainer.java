e76810a8d2da5a7e86201c24f5491ead2a0d3059
vortechs <jobartbenjamin@gmail.com>
2025-02-19 18:25:57 -0800

    made it on true and updated some values

--- src/main/java/frc/robot/RobotContainer.java
+++ src/main/java/frc/robot/RobotContainer.java
@@ -267,8 +267,11 @@ public class RobotContainer {
     // Switch to X pattern when X button is pressed
     // controller.x().onTrue(Commands.runOnce(drive::stopWithX, drive));
 
-    controller.x().onTrue(new PathfindToClosestDepotCommand(drive, true));
-    controller.y().onTrue(new PathfindToClosestDepotCommand(drive, false));
+    controller
+        .x()
+        .whileTrue(
+            new PathfindToClosestDepotCommand(drive, true));
+    controller.y().whileTrue(new PathfindToClosestDepotCommand(drive, false));
 
     // Reset gyro to 0° when B button is pressed
     controller
